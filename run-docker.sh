a433c4b83cc231d792495fbece8511d4ff222244
Ishtiaque Daudpota <43311558+ishtiaquedaudpota@users.noreply.github.com>
2019-01-24 16:59:37 +0800

    Update run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 
 # Either add the following parameter values in the Jenkins job or directly add in the script
-TARGET_URL=${TARGET_URL}
 PROXY_HOST=${PROXY_HOST}
 PROXY_PORT=${PROXY_PORT}
+TARGET_URL=${TARGET_URL}
 ZAP_PORT=${ZAP_PORT}
 
 CONTAINER_ID=$(docker run -u zap -p $ZAP_PORT:$ZAP_PORT -d owasp/zap2docker-weekly zap.sh -daemon -port $ZAP_PORT -host 127.0.0.1 -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true -config connection.proxyChain.enabled=true -config connection.proxyChain.hostName=$PROXY_HOST -config connection.proxyChain.port=$PROXY_PORT)
 
-
 docker exec $CONTAINER_ID zap-cli -p $ZAP_PORT status -t 120 && docker exec $CONTAINER_ID zap-cli -p $ZAP_PORT open-url $TARGET_URL
-
 docker exec $CONTAINER_ID zap-cli -p $ZAP_PORT spider $TARGET_URL
-
 docker exec $CONTAINER_ID zap-cli -p $ZAP_PORT active-scan -r $TARGET_URL
-
 docker exec $CONTAINER_ID zap-cli -p $ZAP_PORT alerts
 
 # docker logs [container ID or name]
@@ -26,5 +22,4 @@ printf "$divider"
 printf "\n"
 
 docker logs $CONTAINER_ID
-
 docker stop $CONTAINER_ID
